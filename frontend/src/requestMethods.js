e626471ffa3efae1f18c4179453babce2d1728d9
Arinze-Kevin <egboguarinze@gmail.com>
2023-02-09 22:39:54 +0100

    Made some few changes

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,10 +1,4 @@
 import axios from "axios";
-import { token } from "./redux/apiCalls";
-
-// const user = JSON.parse(localStorage.getItem('persist:root'));
-// const currentUser = JSON.parse(user?.user || '');
-// const auth = currentUser?.currentUser?.user?.tokens?.slice(-1)
-// console.log('authhhhh',auth)
 
 const BASE_URL = "https://ecommerce-app-79uw.onrender.com/";
 // const TOKEN = !auth ? '' : auth[0].token
